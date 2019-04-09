70651627b78992ebf7ec1304142b5b94f1083134
Ryyder <brucecho82@hotmail.com>
2019-04-08 20:34:28 -0700

    got open weather map api calls working

--- assets/javascript/app.js
+++
@@ -1,6 +0,0 @@
-var weatherAPIKey = "b0162c0944ec2d5f987eb39c7b914117";
-var weatherQueryURL = "api.openweathermap.org/data/2.5/weather";
-var cityInput;
-var zipCodeInput;
-var units = "imperial";
-
