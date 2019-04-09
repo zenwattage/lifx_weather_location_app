f0a8147fa8549975ffd90822e255504ff94f8a30
Ryyder <brucecho82@hotmail.com>
2019-04-08 18:29:29 -0700

    added links to css file, js, and jquery file

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+var weatherAPIKey = "b0162c0944ec2d5f987eb39c7b914117";
+var weatherQueryURL = "api.openweathermap.org/data/2.5/weather";
+var cityInput;
+var zipCodeInput;
+var units = "imperial";
+
