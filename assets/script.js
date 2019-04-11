c9eb0c9b5d59dfb907ad4bea6897182a5f30171d
Ryyder <brucecho82@hotmail.com>
2019-04-11 16:57:48 -0700

    added google maps onto html page

--- assets/script.js
+++ assets/script.js
@@ -80,6 +80,67 @@ function citytoID (city) {
 
 }
 
-$(document).ready(function () {
+//google maps integration
+function initMap() {
+  var map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: -33.8688, lng: 151.2195}, zoom: 13});
+
+  var input = document.getElementById('pac-input');
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+
+  autocomplete.bindTo('bounds', map);
+
+  // Specify just the place data fields that you need.
+  autocomplete.setFields(['place_id', 'geometry', 'name', 'formatted_address']);
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById('infowindow-content');
+  infowindow.setContent(infowindowContent);
+
+  var geocoder = new google.maps.Geocoder;
+
+  var marker = new google.maps.Marker({map: map});
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+  autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    var place = autocomplete.getPlace();
+
+    if (!place.place_id) {
+      return;
+    }
+    geocoder.geocode({'placeId': place.place_id}, function(results, status) {
+      if (status !== 'OK') {
+        window.alert('Geocoder failed due to: ' + status);
+        return;
+      }
+
+      map.setZoom(11);
+      map.setCenter(results[0].geometry.location);
+
+      // Set the position of the marker using the place ID and location.
+      marker.setPlace(
+          {placeId: place.place_id, location: results[0].geometry.location});
+
+      marker.setVisible(true);
+
+      infowindowContent.children['place-name'].textContent = place.name;
+      infowindowContent.children['place-id'].textContent = place.place_id;
+      infowindowContent.children['place-address'].textContent =
+          results[0].formatted_address;
+
+      infowindow.open(map, marker);
+    });
+  });
+}
+
+/* $(document).ready(function () {
   $(".input-error").hide();
 });
+ */
