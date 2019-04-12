53f527a6a9401e230aed34cc9c8e773c7f1c9181
joshua-unseen <corpselight@gmail.com>
2019-04-11 21:07:55 -0700

    pulling config objects out of the main script.js

---
+++ assets/javascript/configs.js
@@ -0,0 +1,16 @@
+var owmConfig = {
+    weatherAPIKey: "b0162c0944ec2d5f987eb39c7b914117",
+    idInput: "2172797",
+    zipCodeInput,
+    units: "imperial",
+    cityID,
+};
+
+var fbConfig = {
+    apiKey: "AIzaSyDd3BtmB_Yt1nkW8WG25orFzOW_hHm84-Y",
+    authDomain: "project1-e4ebd.firebaseapp.com",
+    databaseURL: "https://project1-e4ebd.firebaseio.com",
+    projectId: "project1-e4ebd",
+    storageBucket: "project1-e4ebd.appspot.com",
+    messagingSenderId: "404140451591"
+};
