cb3263c662996025ecb1c17b0e1fe4caa6046b57
joshua-unseen <corpselight@gmail.com>
2019-04-14 15:12:55 -0700

    Firebase auth and access added.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,42 @@
 
 // Initialize Firebase
 firebase.initializeApp(fbConfig);
+var DB = firebase.database();
+
 // Auth
+var uid = "";
 // Using a popup.
 var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
-firebase.auth().signInWithPopup(provider).then(function(result) {
- // This gives you a Google Access Token.
- var token = result.credential.accessToken;
- console.log(token);
- // The signed-in user info.
- var user = result.user;
- console.log(user);
+// provider.addScope('profile');
+// provider.addScope('email');
+firebase.auth().signInWithPopup(provider).then(function (result) {
+    // This gives you a Google Access Token.
+    var token = result.credential.accessToken;
+    console.log(token);
+    // The signed-in user info.
+    var user = result.user;
+    uid = user.uid;
+    // console.log("user id: " + uid);
 });
+
+// Database access stuff.
+var lifxHeaders = "";
+firebase.auth().onAuthStateChanged(function (user) {
+    DB.ref("users/" + user.uid).on("value", function (snap) {
+        if (snap.child("lifx").exists()) {
+            lifxHeaders = snap.child("lifx/headers").val()
+            console.log(lifxHeaders);
+        }
+        else {
+            $("#token-input-modal").modal("show");
+        }
+    });
+});
+
+function SetToken(newToken) {
+  DB.ref("users/" + uid).set({ lifx: { headers: { "Authorization": "Bearer " + newToken } } });
+  $("#token-input-modal").modal("hide");
+}
 
 //our input field...
 $("#pac-input").on("keydown", function search(e) {
